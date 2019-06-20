d808c7d8c8831b03d931a0f402e9adfcbcb605c5
ShiChenYuan <scy9604@gmail.com>
2019-06-21 07:53:48 +0800

    firtst commit

---
+++ rand.go
@@ -0,0 +1,89 @@
+package randgo
+
+const (
+	rngLen   = 607
+	rngTap   = 273
+	rngMax   = 1 << 63
+	rngMask  = rngMax - 1
+	int32max = (1 << 31) - 1
+)
+
+type rngSource struct {
+	tap  int           // index into vec
+	feed int           // index into vec
+	vec  [rngLen]int64 // current feedback register
+}
+
+var globalRng = New(1)
+
+func Rand() int {
+	return int(globalRng.Int63())
+}
+
+func New(v int) *rngSource {
+	rng := new(rngSource)
+	rng.Seed(int64(v))
+
+	return rng
+}
+
+func seedrand(x int32) int32 {
+	const (
+		A = 48271
+		Q = 44488
+		R = 3399
+	)
+
+	hi := x / Q
+	lo := x % Q
+	x = A*lo - R*hi
+	if x < 0 {
+		x += int32max
+	}
+	return x
+
+}
+
+func (rng *rngSource) Seed(seed int64) {
+	rng.tap = 0
+	rng.feed = rngLen - rngTap
+
+	seed = seed % int32max
+	if seed < 0 {
+		seed += int32max
+	}
+	if seed == 0 {
+		seed = 89482311
+	}
+
+	x := int32(seed)
+	for i := -20; i < rngLen; i++ {
+		x = seedrand(x)
+		if i >= 0 {
+			var u int64
+			u = int64(x) << 40
+			x = seedrand(x)
+			u ^= int64(x) << 20
+			x = seedrand(x)
+			u ^= int64(x)
+			rng.vec[i] = u
+		}
+	}
+}
+
+func (rng *rngSource) Int63() int64 {
+	rng.tap--
+	if rng.tap < 0 {
+		rng.tap += rngLen
+	}
+
+	rng.feed--
+	if rng.feed < 0 {
+		rng.feed += rngLen
+	}
+
+	x := rng.vec[rng.feed] + rng.vec[rng.tap]
+	rng.vec[rng.feed] = x
+
+	return int64(uint64(x) & rngMask)
+}
---
+++ rand_test.go
@@ -0,0 +1,24 @@
+package randgo
+
+import (
+	"log"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func Test_encrypt(t *testing.T) {
+	Convey("test Rand", t, func() {
+		generated := make(map[int]bool)
+
+		for i := 0; i < 100000; i++ {
+			e := Rand()
+			_, ok := generated[e]
+			log.Println(e)
+			// So(e, ShouldBeGreaterThan, 0)
+			So(ok, ShouldBeFalse)
+			generated[e] = true
+		}
+	})
+
+}
